650fab6286b75461db87f5531458bff14b12478b
Clara Jiang <claraxqjiang@gmail.com>
2025-05-11 23:26:19 +0800

    Initial the otle demo project

--- openshif.java
+++ openshif.java
@@ -1,50 +1,42 @@
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.IOException;
+import io.fabric8.kubernetes.client.Config;
+import io.fabric8.kubernetes.client.ConfigBuilder;
+import io.fabric8.openshift.api.model.DeploymentConfig;
+import io.fabric8.openshift.client.DefaultOpenShiftClient;
+import io.fabric8.openshift.client.OpenShiftClient;
 
 public class OpenShiftVersionChecker {
 
-    public static String getLatestDeployedVersion(String openShiftUrl, String namespace, String deploymentName, String token) throws IOException {
-        // Build the URL for the deploymentconfig
-        String apiUrl = openShiftUrl + "/apis/apps.openshift.io/v1/namespaces/" + namespace + "/deploymentconfigs/" + deploymentName;
-
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(apiUrl)
-                .addHeader("Authorization", "Bearer " + token)
-                .addHeader("Accept", "application/json")
+    public static String getLatestDeployedVersion(String openShiftUrl, String namespace, String deploymentConfigName, String token) {
+        // Configure the client
+        Config config = new ConfigBuilder()
+                .withMasterUrl(openShiftUrl)
+                .withOauthToken(token)
                 .build();
 
-        try (Response response = client.newCall(request).execute()) {
-            if (!response.isSuccessful()) {
-                throw new IOException("Unexpected response code: " + response);
+        // Create OpenShift client
+        try (OpenShiftClient client = new DefaultOpenShiftClient(config)) {
+            // Get the DeploymentConfig
+            DeploymentConfig dc = client.deploymentConfigs()
+                    .inNamespace(namespace)
+                    .withName(deploymentConfigName)
+                    .get();
+
+            if (dc != null) {
+                // Get the latest version
+                return String.valueOf(dc.getStatus().getLatestVersion());
+            } else {
+                return "DeploymentConfig not found";
             }
-
-            String responseBody = response.body().string();
-            JSONObject deploymentConfig = new JSONObject(responseBody);
-
-            // Get the latest version from the status
-            int latestVersion = deploymentConfig.getJSONObject("status").getInt("latestVersion");
-
-            return String.valueOf(latestVersion);
         }
     }
 
     public static void main(String[] args) {
         String openShiftUrl = "https://your-openshift-url";
         String namespace = "your-project";
-        String deploymentName = "your-deployment";
+        String deploymentConfigName = "your-deploymentconfig";
         String token = "your-token";
 
-        try {
-            String latestVersion = getLatestDeployedVersion(openShiftUrl, namespace, deploymentName, token);
-            System.out.println("Latest deployed version: " + latestVersion);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        String latestVersion = getLatestDeployedVersion(openShiftUrl, namespace, deploymentConfigName, token);
+        System.out.println("Latest deployed version: " + latestVersion);
     }
 }
